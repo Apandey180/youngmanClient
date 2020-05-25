7155de10de706c4e9c85ca8774341a885ce6abf5
Vikas Mahato <vikasmahato0@gmail.com>
2020-05-25 08:14:44 +0530

    Initial commit

---
+++ src/views/Home/HomeCarousel.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="2000"
+      img-height="480"
+      controls
+      indicators
+      background="#ababab"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        v-for="item in carouselItems"
+        :key="item.id"
+        :caption="item.text"
+        :img-src="item.image"
+      ></b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      carouselItems: [
+        {
+          id: 1,
+          image: "https://source.unsplash.com/LAaSoL0LrYs/1920x1080",
+          text: "Nulla vitae elit libero, a pharetra augue mollis interdum. (1)"
+        },
+        {
+          id: 2,
+          image: "https://source.unsplash.com/LAaSoL0LrYs/1920x1080",
+          text: "Nulla vitae elit libero, a pharetra augue mollis interdum. (2)"
+        },
+        {
+          id: 3,
+          image: "https://source.unsplash.com/LAaSoL0LrYs/1920x1080",
+          text: "Nulla vitae elit libero, a pharetra augue mollis interdum. (3)"
+        }
+      ],
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    }
+  }
+};
+</script>
