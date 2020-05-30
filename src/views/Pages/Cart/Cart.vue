259f0a6371f26acf0c0bf7b3cc021f7be3420ddc
Vikas Mahato <vikasmahato0@gmail.com>
2020-05-30 20:02:45 +0530

    Updated cart page

--- src/views/Pages/Cart/Cart.vue
+++ src/views/Pages/Cart/Cart.vue
@@ -4,23 +4,71 @@
       <b-row align-v="right" class="py-4"> </b-row>
     </base-header>
     <b-container fluid class="mt--6">
-         <b-card>
-      <h2>Your Cart</h2>
-      <p v-show="!products.length"><i>Please add some products to cart.</i></p>
-      <ul>
-        <li v-for="product in products" :key="product.id">
-          {{ product.title }} - {{ product.price | currency }} x
-          {{ product.quantity }}
-        </li>
-      </ul>
-      <p>Total: {{ total | currency }}</p>
-      <p>
-          <router-link to="/checkout">
-          <base-button type="primary" :disabled="!products.length">Checkout</base-button>
-          </router-link>
-      </p>
-      <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
-         </b-card>
+      <b-row>
+        <b-col lg="8">
+          <b-card>
+            <h2>Your Cart</h2>
+            <p v-show="!products.length">
+              <i>Please add some products to cart.</i>
+            </p>
+            <ul>
+              <li v-for="product in products" :key="product.id">
+                {{ product.title }} - {{ product.price | currency }} x
+                {{ product.quantity }}
+              </li>
+            </ul>
+            <p>Total: {{ total | currency }}</p>
+            <p>
+              <router-link to="/checkout">
+                <base-button type="primary" :disabled="!products.length"
+                  >Checkout</base-button
+                >
+              </router-link>
+            </p>
+            <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
+          </b-card>
+        </b-col>
+        <b-col lg="4">
+          <card
+            header-classes="bg-transparent"
+            body-classes="px-lg-7"
+            class="card-pricing border-0 text-center mb-4"
+          >
+            <!--Header-->
+            <h4
+              slot="header"
+              class="text-uppercase ls-1 text-primary py-3 mb-0"
+            >
+              Cart Items
+            </h4>
+
+            <!--Body-->
+            <div class="display-2">{{ total | currency }}</div>
+            <ul class="list-unstyled my-4">
+              <li v-for="product in products" :key="product.id">
+                <div class="d-flex align-items-center">
+                  <div>
+                    <div
+                      class="icon icon-xs icon-shape bg-gradient-primary text-white shadow rounded-circle"
+                    >
+                      <i class="fas fa-inventory"></i>
+                    </div>
+                  </div>
+                  <div>
+                    <span class="pl-2 text-sm"
+                      >{{ product.title }} - {{ product.price | currency }} x
+                      {{ product.quantity }}</span
+                    >
+                  </div>
+                </div>
+              </li>
+            </ul>
+
+            <!--Footer-->
+            <a slot="footer" href="#!" class=" text-muted">Back to Cart</a>
+          </card>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
--- src/views/Pages/Checkout/Checkout.vue
+++ src/views/Pages/Checkout/Checkout.vue
@@ -5,8 +5,14 @@
         </base-header>
         <b-container fluid class="mt--6">
         <b-row>
-        <b-col lg="8">
-            <b-card no-body>
+        <b-col lg="12">
+            <div>
+  <b-tabs pills>
+    <b-tab active>
+      <template v-slot:title>
+        <i class="ni ni-cloud-upload-96 mr-2"></i> Home
+      </template>
+      <b-card no-body>
               <!-- Card header -->
               <b-card-header>
                 <h2 class="mb-0">Checkout</h2>
@@ -33,40 +39,27 @@
                 </form>
               </b-card-body>
             </b-card>
-        </b-col>
-        <b-col lg="4">
-            <card
-            header-classes="bg-transparent"
-            body-classes="px-lg-7"
-            class="card-pricing border-0 text-center mb-4">
+    </b-tab>
 
-            <!--Header-->
-            <h4 slot="header" class="text-uppercase ls-1 text-primary py-3 mb-0">Cart Items</h4>
+    <b-tab>
+      <template v-slot:title>
+        <i class="ni ni-bell-55 mr-2"></i> Profile
+      </template>
+      <p class="p-3">Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+    </b-tab>
 
-            <!--Body-->
-            <div class="display-2">{{ total | currency }}</div>
-            <ul class="list-unstyled my-4">
-              
-              <li v-for="product in products" :key="product.id">
-          
-          <div class="d-flex align-items-center">
-                  <div>
-                    <div class="icon icon-xs icon-shape bg-gradient-primary text-white shadow rounded-circle">
-                      <i class="fas fa-inventory"></i>
-                    </div>
-                  </div>
-                  <div>
-                    <span class="pl-2 text-sm">{{ product.title }} - {{ product.price | currency }} x
-          {{ product.quantity }}</span>
-                  </div>
-                </div>
-        </li>
-            </ul>
+    <b-tab>
+      <template v-slot:title>
+        <i class="ni ni-calendar-grid-58 mr-2"></i> Messages
+      </template>
+      <p class="p-3">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth.</p>
+    </b-tab>
+  </b-tabs>
+</div>
+
             
-            <!--Footer-->
-            <a slot="footer" href="#!" class=" text-muted">Back to Cart</a>
-          </card>
         </b-col>
+        
         </b-row>
         </b-container>
     </div>
