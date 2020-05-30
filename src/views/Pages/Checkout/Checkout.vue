b7ffdef3da62abb94005287a4ee3310d5b570f77
Vikas Mahato <vikasmahato0@gmail.com>
2020-05-30 20:24:29 +0530

    Added Checkout page

--- src/views/Pages/Checkout/Checkout.vue
+++ src/views/Pages/Checkout/Checkout.vue
@@ -7,50 +7,24 @@
         <b-row>
         <b-col lg="12">
             <div>
-  <b-tabs pills>
+  <b-tabs pills class="nav-pills-circle">
     <b-tab active>
       <template v-slot:title>
-        <i class="ni ni-cloud-upload-96 mr-2"></i> Home
+        <i class="ni ni-cloud-upload-96 mr-2"></i>
       </template>
-      <b-card no-body>
-              <!-- Card header -->
-              <b-card-header>
-                <h2 class="mb-0">Checkout</h2>
-              </b-card-header>
-              <!-- Card body -->
-              <b-card-body>
-                <form>
-                  <base-input label="First Name"/>
-                  <base-input label="Last Name"/>
-                  <base-input label="Email address"/>
-                  <base-input label="Phone number"/>
-                  <base-input label="Address"/>
-                  <base-input label="City"/>
-                  <base-input label="State"/>
-                  <base-input label="Pincode"/>
-                  <b-form-checkbox class="mb-3" v-model="save_to_billing_address">
-                        Save this to Billing address
-                      </b-form-checkbox>
-
-                  
-                  <router-link to="/">
-                    <base-button type="primary" class="mb-3">Continue to Payment</base-button>
-                  </router-link>
-                </form>
-              </b-card-body>
-            </b-card>
+      <customer-details/>
     </b-tab>
 
     <b-tab>
       <template v-slot:title>
-        <i class="ni ni-bell-55 mr-2"></i> Profile
+        <i class="ni ni-cloud-upload-96 mr-2"></i>
       </template>
       <p class="p-3">Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
     </b-tab>
 
     <b-tab>
       <template v-slot:title>
-        <i class="ni ni-calendar-grid-58 mr-2"></i> Messages
+        <i class="ni ni-calendar-grid-58 mr-2"></i>
       </template>
       <p class="p-3">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth.</p>
     </b-tab>
@@ -68,12 +42,16 @@
 <script>
 
   import { mapGetters, mapState } from "vuex";
+  import CustomerDetails from './CustomerDetails';
 export default {
     data() {
         return {
             save_to_billing_address: true
         };
         
+    },
+    components: {
+        CustomerDetails
     },
   computed: {
     ...mapState({
---
+++ src/views/Pages/Checkout/CustomerDetails.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-card no-body>
+              <!-- Card header -->
+              <b-card-header>
+                <h2 class="mb-0">Customer Details</h2>
+              </b-card-header>
+              <!-- Card body -->
+              <b-card-body>
+                <form>
+                  
+                  
+
+                  
+                  <router-link to="/">
+                    <base-button type="primary" class="mb-3">Next</base-button>
+                  </router-link>
+                </form>
+              </b-card-body>
+            </b-card>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    }
+}
+</script>
