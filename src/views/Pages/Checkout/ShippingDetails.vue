3adbcbe09ed8ef0810e7e99aa1adf0d5d176fba9
Vikas Mahato <vikasmahato0@gmail.com>
2020-05-30 21:56:13 +0530

    Updated checkout page

--- src/views/Pages/Checkout/ShippingDetails.vue
+++ src/views/Pages/Checkout/ShippingDetails.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-    </div>
+    <b-card no-body>
+              <!-- Card header -->
+              <b-card-header>
+                <h2 class="mb-0">Shipping Details</h2>
+              </b-card-header>
+              <!-- Card body -->
+              <b-card-body>
+                <form>
+                  
+                  
+
+                  
+                  <router-link to="/">
+                    <base-button type="primary" class="mb-3">Next</base-button>
+                  </router-link>
+                </form>
+              </b-card-body>
+            </b-card>
 </template>
 <script>
 export default {
     data() {
         return {
-            
+
         };
     }
 }
