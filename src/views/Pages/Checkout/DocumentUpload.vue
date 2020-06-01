e6baa34d83067b4ec6793eb9ef07ab7e65f0a528
apandey180 <apandey@youngman.co.in>
2020-06-01 12:12:08 +0530

    DocumentUpload pand and ShippingDetails Page

--- src/views/Pages/Checkout/DocumentUpload.vue
+++ src/views/Pages/Checkout/DocumentUpload.vue
@@ -7,8 +7,124 @@
               <!-- Card body -->
               <b-card-body>
                 <form>
+                   <b-row>
+                    <b-col lg="6" sm="12">
+                        
+                        <b-card no-body>
+              <!-- Card header -->
+              <b-card-header class="card-header">
+                <h3 class="mb-0">Photo ID Proof(Purchase):</h3>
+              </b-card-header>
+              <!-- Card body -->
+              <b-card-body class="card-body">
+                <b-form>
+                  <h3>Drag and drop files here to</h3>
+                   or 
+                  <b-form-file
+                    @change="filesChange"
+                    placeholder="Select file"
+                    drop-placeholder="Drop file here..."
+                  ></b-form-file>
+                </b-form>
+              </b-card-body>
+            </b-card>
+            
+
+                    </b-col>
+                    
+					<b-col lg="6" sm="12">
+                        <b-card no-body>
+              <!-- Card header -->
+              <b-card-header class="card-header">
+                <h3 class="mb-0">GST Certificate</h3>
+              </b-card-header>
+              <!-- Card body -->
+              <b-card-body class="card-body">
+                <b-form>
                   
+                  <h3>Drag and drop files here to</h3>
+                   or 
+                  <b-form-file
+                    @change="filesChange"
+                    placeholder="Select file"
+                    drop-placeholder="Drop file here..."
+                  ></b-form-file>
+                </b-form>
+              </b-card-body>
+            </b-card>
+            
+
+
+                    </b-col>
+                    </b-row>
+<!-- //new row -->
+
+ <b-row>
+                    <b-col lg="6" sm="12">
+            <b-card no-body>
+              <!-- Card header -->
+              <b-card-header class="card-header">
+                <h3 class="mb-0">Security Check</h3>
+              </b-card-header>
+              <!-- Card body -->
+              <b-card-body class="card-body">
+                <b-form>
                   
+                  <h3>Drag and drop files here to</h3>
+                   or 
+                  <b-form-file
+                    @change="filesChange"
+                    placeholder="Select file"
+                    drop-placeholder="Drop file here..."
+                  ></b-form-file>
+                </b-form>
+              </b-card-body>
+            </b-card>
+                        
+
+                    </b-col>
+                    
+					<b-col lg="6" sm="12">
+                        
+<b-card no-body>
+              <!-- Card header -->
+              <b-card-header class="card-header">
+                <h3 class="mb-0">Photograph</h3>
+              </b-card-header>
+              <!-- Card body -->
+              <b-card-body class="card-body">
+                <b-form>
+                  
+                  <h3>Drag and drop files here to</h3>
+                   or 
+                  <b-form-file
+                    @change="filesChange"
+                    placeholder="Select file"
+                    drop-placeholder="Drop file here..."
+                  ></b-form-file>
+                </b-form>
+              </b-card-body>
+            </b-card>
+            
+
+                    </b-col>
+
+
+                   
+                    </b-row>
+<center>
+  <div>
+  <b-form-checkbox 
+                       id="checkbox-1"
+                       v-model="status"
+                      name="checkbox-1"
+                      value="accepted"
+                      unchecked-value="not_accepted"
+    >
+      I accept following Terms& Conditions in Security Acceptance letter
+    </b-form-checkbox></div>
+</center>
+
                 </form>
               </b-card-body>
             </b-card>
@@ -17,6 +133,10 @@
 export default {
     data() {
         return {
+
+          checkboxes: {
+           status: 'not_accepted'
+         }
 
         };
     }
--- src/views/Pages/Checkout/ShippingDetails.vue
+++ src/views/Pages/Checkout/ShippingDetails.vue
@@ -7,8 +7,38 @@
               <!-- Card body -->
               <b-card-body>
                 <form>
-                  
-                  
+                  <b-row>
+                    <b-col lg="6" sm="12">
+                        
+
+                        <base-input label="Consignee" input-classes="form-control-sm" placeholder="Consignee"/>
+                    <base-input label="GSTIN" input-classes="form-control-sm" placeholder="GSTIN" />
+                    <base-input label="Site Contact Person Name" input-classes="form-control-sm" placeholder="Site Contact Person Name" />
+                    <base-input label="Email" input-classes="form-control-sm" placeholder="Email" />
+                    <base-input label="Mobile Number" input-classes="form-control-sm" placeholder="Mobile Number" />
+                    <base-input label="Shipping Address">
+                    <textarea class="form-control" id="shippingAddress" rows="3"></textarea>
+                    </base-input>
+                    <base-input label="State">
+                         <select class="form-control" id="state">
+                           <option></option>
+                           <option></option>
+                           <option></option>
+                           <option></option>
+                           <option></option>
+                          </select>
+                        </base-input>
+                    <base-input label="Pincode" input-classes="form-control-sm" placeholder="Pincode" />
+
+                    </b-col>
+                    
+					          <b-col lg="6" sm="12">
+                        
+                     <h2>3000</h2>
+
+                    </b-col>
+                    </b-row>   
+
                 </form>
               </b-card-body>
             </b-card>
