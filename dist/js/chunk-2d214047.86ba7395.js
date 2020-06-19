8c74b26330f9585307eb6c0fb46a4d340f03c295
Vikas Mahato <vikasmahato0@gmail.com>
2020-06-19 16:49:36 +0530

    updated dist

--- dist/js/chunk-2d214047.86ba7395.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d214047"],{aec0:function(n,u,c){"use strict";c.r(u);var e,l,a=c("2877"),o={},p=Object(a["a"])(o,e,l,!1,null,null,null);u["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d214047.86ba7395.js.map
