a8a2339b0198f0b1a189bccc1897bc7f2ac36b61
YoungmanIndia <jitendraruley@gmail.com>
2020-06-22 20:22:32 +0530

    header

--- src/views/Pages/Home/FeaturedCategories.vue
+++ src/views/Pages/Home/FeaturedCategories.vue
@@ -11,8 +11,13 @@
                            :options="options">
         <template v-slot:default="{item}">
          
-            <div class="col-sm-4 col-lg-4 overflow-hidden">
-              <img :src="item.image"/>
+            <div class="col-sm-4 col-lg-4 overflow-hidden img-container">
+              <router-link to="/products">
+                <img :src="item.image" class="image"/>
+                <div class="middle">
+                  <div class="text">{{item.name}}</div>
+                </div>
+              </router-link>
             </div>
         </template>
       </horizontal-list>
@@ -66,4 +71,37 @@
     flex: 0 0 25%;
     max-width: 25%;
   }
+  .image {
+  opacity: 1;
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: .5s ease;
+  backface-visibility: hidden;
+}
+
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.img-container:hover .image {
+  opacity: 0.3;
+}
+
+.img-container:hover .middle {
+  opacity: 1;
+}
+
+.text {
+  color: black;
+  font-size: 16px;
+  padding: 16px 32px;
+}
 </style>
