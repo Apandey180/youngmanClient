5e0a01009a34af04b7ce461bc0d8171c70685232
Vikas Mahato <vikasmahato0@gmail.com>
2020-06-23 12:41:02 +0530

    more responsiveness to featured

--- src/views/Pages/Home/FeaturedCategories.vue
+++ src/views/Pages/Home/FeaturedCategories.vue
@@ -38,7 +38,7 @@
   data() {
     return {
       options: {
-        responsive: [{end: 576, size: 1}, {start: 576, end: 768, size: 2},{start: 768, end: 992, size: 3},{size: 4}],
+        responsive: [{size: 4}],
         item: {class: 'col-sm-4 col-lg-4'},
         list: {
             // 1200 because @media (min-width: 1200px) and therefore I want to switch to windowed mode
